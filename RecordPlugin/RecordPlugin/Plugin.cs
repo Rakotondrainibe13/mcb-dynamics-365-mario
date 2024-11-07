da1001cd84bfc15abd90091de6fd889d8e7a73bb
Sambatra Mario <89396504+Rakotondrainibe13@users.noreply.github.com>
2024-11-07 20:21:16 +0300

    [Task2] 1- Plugins project record

---
+++ RecordPlugin/RecordPlugin/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace RecordPlugin
+{
+    public class Plugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            if (context.InputParameters.Contains("email") && context.InputParameters.Contains("reason"))
+            {
+                var emailAddress = context.InputParameters["email"].ToString();
+                var declineReason = context.InputParameters["reason"].ToString();
+
+                var declineRecord = new Entity("InviteDecline")
+                {
+                    ["email"] = emailAddress,
+                    ["reason"] = declineReason,
+                    ["decline_date"] = DateTime.UtcNow
+                };
+                service.Create(declineRecord);
+            }
+            else
+            {
+                throw new InvalidPluginExecutionException("The parameters email and reason are required");
+            }
+        }
+    }
+}
---
+++ RecordPlugin/RecordPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("RecordPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("RecordPlugin")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("e99283c1-3080-4e70-8e37-c3f1459017b4")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
