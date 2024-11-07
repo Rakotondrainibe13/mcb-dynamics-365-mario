29e5499370630b83982963ea57700c7edd175ebc
Sambatra Mario <89396504+Rakotondrainibe13@users.noreply.github.com>
2024-11-07 21:15:25 +0300

    [task2] config app

---
+++ angular-application/src/app/api/config.ts
@@ -0,0 +1,4 @@
+export const client_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+export const tenant_id = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+export const client_id = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+export const url = 'https://org7c730ca8.crm4.dynamics.com/';
